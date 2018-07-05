db7ea0c3e9031432eaebebdec07a18fd08337a3c
Yuliia <uctivam.nic@gmail.com>
2018-07-05 11:31:20 +0300

    added some styles to page sections

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,17 @@
-$('.carousel').carousel({
-  interval: false
-})
+$(function() {
+
+  "use strict";
+
+  var topoffset = 50; //variable for menu height
+
+  //Activate Scrollspy
+  $('body').scrollspy({
+    target: 'header .navbar',
+    offset: topoffset
+  });
+
+ $('.carousel').carousel({
+    interval: false
+  });
+
+});
